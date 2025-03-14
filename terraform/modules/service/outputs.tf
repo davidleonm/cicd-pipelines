d996d4e1e68578450d364e0a5e72baf14a19f591
David <david.leon.m@gmail.com>
2025-03-14 11:08:59 +0100

    outputs

--- terraform/modules/service/outputs.tf
+++ terraform/modules/service/outputs.tf
@@ -1,4 +1,9 @@
-output "service_name" {
-  description = "Name of the service"
-  value       = kubernetes_service.service.metadata[0].name
+output "fully_qualified_name" {
+  description = "FQDM of the service"
+  value       = "${var.name}.${var.namespace}.svc.cluster.local"
+}
+
+output "container_port" {
+    description = "Internal port of the container"
+    value       = var.container_port
 }
