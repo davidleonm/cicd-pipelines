a1152d03c21b8e51e04cdfe9410cf3a5ac16c3a5
David <david.leon.m@gmail.com>
2025-03-21 13:16:51 +0100

    grafana

---
+++ provider.tf
@@ -0,0 +1,27 @@
+provider "kubernetes" {}
+
+terraform {
+  required_version = ">= 1.10.0"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.34.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.17.0"
+    }
+  }
+
+  backend "kubernetes" {}
+}
+
+provider "helm" {
+  kubernetes {
+    # Replace this with values that provide connection to your cluster
+    config_path    = "~/.kube/config"
+    config_context = "microk8s"
+  }
+}
