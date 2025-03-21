f13e117834dd6233897e84af9f63c819f914d596
David <david.leon.m@gmail.com>
2025-03-21 13:19:18 +0100

    grafana

--- provider.tf
+++ provider.tf
@@ -21,7 +21,7 @@ terraform {
 provider "helm" {
   kubernetes {
     # Replace this with values that provide connection to your cluster
-    config_path    = "~/.kube/config"
+    config_path    = ".kube/config"
     config_context = "microk8s"
   }
 }
